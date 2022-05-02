8c3fff56872a5d33dcd9873c2a590520c4c4a55e
tiagopierre <tiago_pierre@outlook.com>
2022-05-02 16:49:40 -0300

    Utilizando proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/43191667?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Tiago Pierre</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
